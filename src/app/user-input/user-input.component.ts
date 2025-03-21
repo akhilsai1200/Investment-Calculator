0efb5e1815ea8e1342f7da940cce5a463c374147
Akhilsai Chidura <akhilsai1200@gmail.com>
2025-03-21 20:46:39 +0530

    Added service

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { InputInvestment } from '../input-investment.model';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
@@ -10,18 +10,24 @@ import { InputInvestment } from '../input-investment.model';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-  @Output() calculate = new EventEmitter<InputInvestment>();
   enteredInitialInvestment='0';
   enteredAnnualInvestment='0';
   enteredEstimatedReturn='5';
   enteredDuration='10';
+  constructor(private investmentService:InvestmentService){
 
+  }
   OnSubmit(){
-    this.calculate.emit({
+    this.investmentService.calculateInvestmentResults
+    ({
       initialInvestment:+this.enteredInitialInvestment,
       duration:+this.enteredDuration,
       expectedReturn:+this.enteredEstimatedReturn,
       annualInvestment:+this.enteredAnnualInvestment
-    })
+    });
+    this.enteredInitialInvestment='0';
+    this.enteredAnnualInvestment='0';
+    this.enteredEstimatedReturn='5';
+    this.enteredDuration='10';
   }
 }
